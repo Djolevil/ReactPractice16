de7ce041e1dfa0f2e921a39193ae0945686f831c
Djordje Knezevic <starwarrior@mycity.rs>
2022-05-18 16:53:24 +0200

    Add authSlice and work with multiple state slices in redux store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Fragment } from 'react';
+
 import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+
 
 
 function App() {
   return (
-    <Counter />
-  );
+    <Fragment>
+      <Header />
+      <Auth />
+      <Counter />
+    </Fragment>
+  )
 }
 
 export default App;
